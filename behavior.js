9ca855147ed665bfd7aac6310f9cc29020fecf7e
Jim Kang <jimkang@gmail.com>
2016-11-11 21:17:26 -0500

    Try to cut some cross-talk.

--- behavior.js
+++ behavior.js
@@ -1,7 +1,7 @@
 var behavior = {
   secondsToWaitBetweenRepliesToSameUser: 20,
   counterExpirationSeconds: 5 * 60,
-  maxRepliesInCounterLifetime: 3,
+  maxRepliesInCounterLifetime: 2,
   secondsToWaitBetweenChimeIns: 12 * 60 * 60
 };
 
